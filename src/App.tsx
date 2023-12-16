dbfed5a2439789c920d9ddeddd9c52fe34842c36
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-12-16 16:30:41 +0200

    Views

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import PrivateRoute from "./components/authentication/auth/PrivateRoute"
 import PrefetchPrograms from "./components/authentication/auth/PrefetchPrograms"
 import TeacherLayout from "./components/TeacherLayout"
 import Lesson from "./components/studentInterface/programs/Current/Lesson"
+import { BrowserView, MobileView } from "react-device-detect"
 const TeacherInterfaceTeacherProfile = lazy(() => import("./components/teacherInterface/teacherProfile/TeacherInterfaceTeacherProfile"))
 const TeacherProgramsPage = lazy(() => import("./components/teacherInterface/programs/TeacherProgramsPage"))
 const ExamBank = lazy(() => import("./components/studentInterface/exambank/ExamBank"))
@@ -25,157 +26,164 @@ const queryClient = new QueryClient()
 function App() {
 	
 	return (
-		<QueryClientProvider client={queryClient}>
-			<AuthProvider>
-				<Routes>
-					<Route element={<PrefetchPrograms />}>
-						<Route path='/' element={
-							<PrivateRoute>
-								{{
-									student: <Layout />,
-									teacher: <TeacherLayout />
-								}}
-							</PrivateRoute>
-						}>
-								<Route index element={
-									<PrivateRoute>
-										{{
-											student: <Suspense>
-														<StudentProfile />
-													</Suspense>,
-											teacher: <Suspense>
-														<TeacherInterfaceTeacherProfile />
-													</Suspense>
-										}}
-									</PrivateRoute>
-								} />
-								<Route path='teacherprofile'>
-									<Route index element={
-										<PrivateRoute>
-												{{
-													student: <Suspense>
-																<TeacherProfile />
-															</Suspense>
-												}}
-										</PrivateRoute>
-									} />
-								</Route>
-								
-								<Route path='programs'>
+		<>
+		<BrowserView>
+			<QueryClientProvider client={queryClient}>
+				<AuthProvider>
+					<Routes>
+						<Route element={<PrefetchPrograms />}>
+							<Route path='/' element={
+								<PrivateRoute>
+									{{
+										student: <Layout />,
+										teacher: <TeacherLayout />
+									}}
+								</PrivateRoute>
+							}>
 									<Route index element={
 										<PrivateRoute>
 											{{
 												student: <Suspense>
-															<Programs />
+															<StudentProfile />
 														</Suspense>,
 												teacher: <Suspense>
-															<TeacherProgramsPage />
+															<TeacherInterfaceTeacherProfile />
 														</Suspense>
 											}}
 										</PrivateRoute>
 									} />
-								</Route>
+									<Route path='teacherprofile'>
+										<Route index element={
+											<PrivateRoute>
+													{{
+														student: <Suspense>
+																	<TeacherProfile />
+																</Suspense>
+													}}
+											</PrivateRoute>
+										} />
+									</Route>
+									
+									<Route path='programs'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student: <Suspense>
+																<Programs />
+															</Suspense>,
+													teacher: <Suspense>
+																<TeacherProgramsPage />
+															</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
 
-								<Route path='teacherprofile/:id'>
+									<Route path='teacherprofile/:id'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student:
+														<Suspense>
+															<TeacherProfile />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
+									
+								<Route path='login'>
+									<Route index element={<Login />} />
+								</Route>
+								<Route path='signup'>
 									<Route index element={
-										<PrivateRoute>
-											{{
-												student:
-													<Suspense>
-														<TeacherProfile />
-													</Suspense>
-											}}
-										</PrivateRoute>
+										<Suspense>
+											<Signup />
+										</Suspense>
 									} />
 								</Route>
-								
-							<Route path='login'>
-								<Route index element={<Login />} />
+									<Route path='exam/:id'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student:
+														<Suspense>
+															<Exam />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
+									<Route path='quiz/:id'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student:
+														<Suspense>
+															<Quiz />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
+									<Route path='assessment/:id'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student: 
+														<Suspense>
+															<Assessment />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
+									<Route path='lesson/:id'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student: 
+														<Suspense>
+															<Lesson />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
+									<Route path='knowledgebank'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student:
+														<Suspense>
+															<KnowledgeBank />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
+									<Route path='exambank'>
+										<Route index element={
+											<PrivateRoute>
+												{{
+													student: 
+														<Suspense>
+															<ExamBank />
+														</Suspense>
+												}}
+											</PrivateRoute>
+										} />
+									</Route>
 							</Route>
-							<Route path='signup'>
-								<Route index element={
-									<Suspense>
-										<Signup />
-									</Suspense>
-								} />
-							</Route>
-								<Route path='exam/:id'>
-									<Route index element={
-										<PrivateRoute>
-											{{
-												student:
-													<Suspense>
-														<Exam />
-													</Suspense>
-											}}
-										</PrivateRoute>
-									} />
-								</Route>
-								<Route path='quiz/:id'>
-									<Route index element={
-										<PrivateRoute>
-											{{
-												student:
-													<Suspense>
-														<Quiz />
-													</Suspense>
-											}}
-										</PrivateRoute>
-									} />
-								</Route>
-								<Route path='assessment/:id'>
-									<Route index element={
-										<PrivateRoute>
-											{{
-												student: 
-													<Suspense>
-														<Assessment />
-													</Suspense>
-											}}
-										</PrivateRoute>
-									} />
-								</Route>
-								<Route path='lesson/:id'>
-									<Route index element={
-										<PrivateRoute>
-											{{
-												student: 
-													<Suspense>
-														<Lesson />
-													</Suspense>
-											}}
-										</PrivateRoute>
-									} />
-								</Route>
-								<Route path='knowledgebank'>
-									<Route index element={
-										<PrivateRoute>
-											{{
-												student:
-													<Suspense>
-														<KnowledgeBank />
-													</Suspense>
-											}}
-										</PrivateRoute>
-									} />
-								</Route>
-								<Route path='exambank'>
-									<Route index element={
-										<PrivateRoute>
-											{{
-												student: 
-													<Suspense>
-														<ExamBank />
-													</Suspense>
-											}}
-										</PrivateRoute>
-									} />
-								</Route>
 						</Route>
-					</Route>
-				</Routes>
-			</AuthProvider>
-		</QueryClientProvider>
+					</Routes>
+				</AuthProvider>
+			</QueryClientProvider>
+		</BrowserView>
+		<MobileView>
+			<h1>No Mobile View!</h1>
+		</MobileView>
+		</>
 	)
 }
 
