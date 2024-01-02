30fc852ad736f4a44377e36fd82f4f01933df6ac
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-01-02 15:33:30 +0200

    Course

--- src/components/helpers/setLessonData.tsx
+++ src/components/helpers/setLessonData.tsx
@@ -3,15 +3,15 @@ import { db } from "../../firebase/firebaseConfig"
 import { getStorage, ref, uploadBytes } from "firebase/storage"
 
 export const setLessonData = async(title: string, description: string, lesson?: unknown, course?: unknown, file?: unknown, fileType?: unknown, order?: number) => {
+    const storage = getStorage();
+    const storagePath = fileType === 'video/mp4' ? 'Videos/' : 'Pdfs/';
+    //@ts-expect-error file
+    const storageRef = ref(storage, storagePath + file.name);
+    
     if(lesson)
     {
         //@ts-expect-error course
         const lessonDoc = doc(db, 'lessons', lesson.id)
-
-        const storage = getStorage();
-        const storagePath = fileType === 'video/mp4' ? 'Videos/' : 'Pdfs/';
-        //@ts-expect-error file
-        const storageRef = ref(storage, storagePath + file.name);
 
         //@ts-expect-error file
         await uploadBytes(storageRef, file)
@@ -41,7 +41,12 @@ export const setLessonData = async(title: string, description: string, lesson?: 
                 duration: '1 Hour 55 Minutes',
                 createdAt: Timestamp.now(),
                 //@ts-expect-error course
-                courseId: course.id
+                courseId: course.id,
+                content: {
+                    type: storagePath,
+                    //@ts-expect-error file
+                    content: file.name
+                }
             }
 
             const addedLesson = await addDoc(lessonsRef, newLesson)
--- src/components/teacherInterface/programs/ComponentCardEdit/ComponentCardEditLesson.tsx
+++ src/components/teacherInterface/programs/ComponentCardEdit/ComponentCardEditLesson.tsx
@@ -11,7 +11,7 @@ export default function ComponentCardEditLesson({ course, setEdited, lesson, ord
     const [title, setTitle] = useState(lesson?.title ?? '')
     const [description, setDescription] = useState(lesson?.description ?? '')
     const [file, setFile] = useState(lesson ? {name: lesson?.content?.content} : {});
-    const [fileType, setFileType] = useState(lesson?.content.type === 'Videos/' ? 'video/mp4' : lesson?.content?.type === 'Pdfs/' ? 'pdf' : '');
+    const [fileType, setFileType] = useState(lesson?.content?.type === 'Videos/' ? 'video/mp4' : lesson?.content?.type === 'Pdfs/' ? 'pdf' : '');
     const [error, setError] = useState('')
 
     useEffect(() => {
