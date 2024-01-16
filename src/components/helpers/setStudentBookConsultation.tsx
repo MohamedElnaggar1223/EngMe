df512d5dbe3c204947c62b5f798ecd6da0f343af
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-01-16 10:34:51 +0200

    Private Consultations

--- src/components/helpers/setStudentBookConsultation.tsx
+++ src/components/helpers/setStudentBookConsultation.tsx
@@ -113,7 +113,7 @@ export const setStudentBookConsultation = async(studentId: string, teacherId: st
 
             if(response.data.link)
             {
-                window.open(response.data.link, '_blank')
+                window.location.href = response.data.link
             }
             else
             {
