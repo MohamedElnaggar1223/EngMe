f2236d2c29eecf858c03be2c17ea225057dd72bc
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-01-16 10:26:50 +0200

    Private Consultations

--- src/components/helpers/setStudentRepurchaseProgram.tsx
+++ src/components/helpers/setStudentRepurchaseProgram.tsx
@@ -4,9 +4,13 @@ import ProgramProps from "../../interfaces/ProgramProps";
 
 export const setStudentRepurchaseProgram = async(studentId: string, program: ProgramProps) => {
     const studentProgramRef = collection(db, 'studentProgram')
+    const studentRequestProgramRef = collection(db, 'studentRequestProgram')
     const queryStudentProgram = query(studentProgramRef, and(where('studentId', '==', studentId), where('programId', '==', program.id)))
+    const queryStudentReuqestProgram = query(studentRequestProgramRef, and(where('studentId', '==', studentId), where('programId', '==', program.id)))
 
     const studentProgramDocs = await getDocs(queryStudentProgram)
+    const studentReuqestProgramDocs = await getDocs(queryStudentReuqestProgram)
 
     if(studentProgramDocs.docs.length) await deleteDoc(studentProgramDocs.docs[0].ref)
+    if(studentReuqestProgramDocs.docs.length) await deleteDoc(studentReuqestProgramDocs.docs[0].ref)
 }
